d3e68925967c97da4185394e831365ef0769f662
Denis <blinovda1@mail.ru>
2023-09-02 17:57:31 +0300

    48:53

---
+++ task_1.py
@@ -0,0 +1,16 @@
+name = 'Alex'
+age = None
+
+a = 42
+print(id(a))
+
+a = 'Hello world'
+print(id(a))
+
+print(name, age, a, '456', 'text', sep=' (=^.^=) ', end='#')
+print('Any text', end='\n')
+
+res = input('Print your text: ')
+print('Ты написал', res)
+
+48:53
