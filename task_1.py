3cd87948f686e9c2a54bc80ea4a4fea9a706a67e
Denis <blinovda1@mail.ru>
2023-09-03 17:28:14 +0300

    1:12

--- task_1.py
+++ task_1.py
@@ -1,16 +1,26 @@
-name = 'Alex'
-age = None
+# name = 'Alex'
+# age = None
 
-a = 42
-print(id(a))
+# a = 42
+# print(id(a))
 
-a = 'Hello world'
-print(id(a))
+# a = 'Hello world'
+# print(id(a))
 
-print(name, age, a, '456', 'text', sep=' (=^.^=) ', end='#')
-print('Any text', end='\n')
+# print(name, age, a, '456', 'text', sep=' (=^.^=) ', end='#')
+# print('Any text', end='\n')
 
-res = input('Print your text: ')
-print('Ты написал', res)
+# res = input('Print your text: ')
+# print('Ты написал', res)
 
-48:53
+# ADULT = 18
+# age = int(input('Сколько тебе лет ? '))
+# how_old = ADULT - age
+# print(how_old, 'Осталось до 18') 
+
+pwd = 'text'
+res = input('Input password: ')
+if res == pwd:
+    print('Доступ разрешён')
+    print('Но будьте осторожны')
+print('Работа завершена')
